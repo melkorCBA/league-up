a8da1e2a3dbe1de4b6285bd00117bd32998d6b4a
melkorCba <BashikaDS@compello.com>
2022-10-07 08:38:25 +0530

    refactor-code

--- heroku-evening-forest-69342.sh
+++ heroku-evening-forest-69342.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
+# setting node path
+echo 'setting node path';
+PATH="C:/Program Files/nodejs:$PATH";
+
+instance="evening-forest-69342";
+
 # build
-npm run -s dockerbuild Dockerfile registry.heroku.com/evening-forest-69342/web;
-# push
-npm run -s heroku-push registry.heroku.com/evening-forest-69342/web;
-# release
-npm run -s heroku-release evening-forest-69342;
+echo 'building image for' $instance;
+npm.cmd run -s dockerbuild Dockerfile registry.heroku.com/$instance/web;
+# # push
+# echo 'pushing image to' $instance;
+# npm.cmd run -s heroku-push registry.heroku.com/$instance/web;
+# # # release
+# echo 'releasing image to' $instance;
+# npm.cmd run -s heroku-release $instance;
