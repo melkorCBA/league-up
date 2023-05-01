d75cf6e6590b55d8e6e775392acea5d2e6c1368c
Chatura Bashika <48211252+melkorCBA@users.noreply.github.com>
2023-05-01 11:52:28 +0530

    Fix/Image Domains issue in next config (#3)
    * fix image domain issue in next config
    
    * refactor code

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [process.env.BaseURL, process.env.ImageBlobURL],
+    domains: process.env.ResourceDomains.split(" "),
   },
   output: "standalone",
   publicRuntimeConfig: {
